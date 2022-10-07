f880367476dde23d47e30068ed35f17265504e65
tygr <graytd1996@gmail.com>
2022-10-07 12:48:23 -0400

    Add event listeners for buttons to update display

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+//All math functions plus operate function to call math functions
 function add(a,b){
     return a + b;
 }
@@ -13,3 +14,24 @@ function divide(a,b){
 function operate(operator,a,b){
     return operator(a,b);
 }
+
+//First grab all the numbers
+
+const numbers = document.querySelectorAll('.number');
+//Grab the display
+const display = document.querySelector('p');
+
+//Add event listener so that when click it updates the display
+numbers.forEach(number =>{
+    number.addEventListener('click',updateDisplay);
+});
+
+//Create a function to update the display
+function updateDisplay(){
+    display.textContent = display.textContent + this.textContent;
+    return a = display.textContent;
+}
+
+//grab the operator buttons
+const operateButtons = document.querySelectorAll('operator');
+
