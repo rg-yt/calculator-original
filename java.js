956570b33a4dc386d8709027580db8c5836833ff
tygr <graytd1996@gmail.com>
2022-10-07 15:07:55 -0400

    Add total when multiple operators press in a row

--- java.js
+++ java.js
@@ -1,37 +1,86 @@
-//All math functions plus operate function to call math functions
-function add(a,b){
-    return a + b;
+let operator;
+let a;
+let b;
+let total;
+displayValue = '';
+function add(a, b) {
+    return +a + +b;
 }
-function subtract(a,b){
+function subtract(a, b) {
     return a - b;
 }
-function multiply(a,b){
+function multiply(a, b) {
     return a * b;
 }
-function divide(a,b){
+function divide(a, b) {
+    if(b==0){
+        return 'What are you doing?'
+    } 
     return a / b;
 }
-function operate(operator,a,b){
-    return operator(a,b);
+function operate(operator, a ,b){
+    if (!operator){
+        return display.textContent;
+    }else if(total){
+        return operator(a = total,b);
+    }else{
+        return operator(a ,b);
+    }
+
 }
-
-//First grab all the numbers
-
 const numbers = document.querySelectorAll('.number');
-//Grab the display
 const display = document.querySelector('p');
-
-//Add event listener so that when click it updates the display
-numbers.forEach(number =>{
-    number.addEventListener('click',updateDisplay);
+numbers.forEach(number => {
+    number.addEventListener('click', updateDisplay);
 });
-
-//Create a function to update the display
-function updateDisplay(){
-    display.textContent = display.textContent + this.textContent;
-    return a = display.textContent;
+function updateDisplay() {
+    
+    displayValue = displayValue + this.textContent;
+    display.textContent = displayValue;
+    b=display.textContent;
+    
 }
-
-//grab the operator buttons
-const operateButtons = document.querySelectorAll('operator');
+const operateButtons = document.querySelectorAll('.operator');
+operateButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayValue = '';
+        if(b && total){
+            total = operate(operator,a,b);
+            display.textContent = total;
+            operator = null;
+        }
+        storeValue();
+        switch (true) {
+            case button.textContent == 'x':
+                operator = multiply;
+                break;
+            case button.textContent == '+':
+                operator = add;
+                break;
+            case button.textContent == '-':
+                operator = subtract;
+                break;
+            case button.textContent == '÷':
+                operator = divide;
+                break;
+            default:
+                operator = null;
+        }  
+    });
+});
+function storeValue(){
+   a = b;
+   if(!total){
+    total = b
+   }
+   b = '';
+   
+}
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', ()=>{
+    displayValue = '';
+    total = operate(operator,a,b);
+    display.textContent = total;
+    operator = null;
+});
 
